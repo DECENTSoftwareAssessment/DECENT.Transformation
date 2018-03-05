977fab15ed687dfe5bb1989e79a4bbb05b3b976e
Philip Makedonski <philip.makedonski@gmail.com>
2018-03-05 15:18:57 +0100

    + bridge skeleton

---
+++ src/epsilon/tools/Bridge.java
@@ -0,0 +1,15 @@
+package epsilon.tools;
+
+public class Bridge {
+
+	public void run(Object o) {
+		System.out.println(o.getClass());
+		//first attempt:
+		  //convert input to Weka?
+		  //run Weka on input
+		  //return output in some reasonable format (object?)
+		  //integrate output into model
+		//alternatively:
+		  //integrate output and modify model in-place?
+	}
+}
